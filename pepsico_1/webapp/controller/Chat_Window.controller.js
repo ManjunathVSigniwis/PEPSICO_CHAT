47e944c28270de8500944189195b579444f2491e
Seema Bera <seemabera25@gmail.com>
2022-05-20 13:03:49 +0000

    New file Add

---
+++ pepsico_1/webapp/controller/Chat_Window.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("sap.pepsi.co.pepsico1.controller.Chat_Window", {
+            onInit: function () {
+
+            }
+        });
+    });
